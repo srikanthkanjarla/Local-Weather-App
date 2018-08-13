2f9c8396ca0562199a14ee03e1761cfdf183f767
srikanth kanjarla <srikanthkanjarla@gmail.com>
2018-08-13 16:49:18 +0530

    add temperature units conversion funcitonality

--- js/main.js
+++ js/main.js
@@ -1,27 +1,55 @@
 // Local weather App - freecodecamp front end project
 ( function(){
-
     var app = {
         httpCon : false,
+        dataArr:[],
+        daysInWeek:['Sun','Mon','Tue','Wed','Thu','Fri','Sat'],
       }
-
-    let days = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
+   //week day and current time
     const date = new Date();
-    const today = days[date.getDay()];
-    const currentTime =date.getHours() + ":" + date.getMinutes();
-    
-   
+    let minutes = date.getMinutes()>9?date.getMinutes() : "0"+date.getMinutes();
+    let timePeriod = date.getHours()<12?'am':'pm;';
+    const today = app.daysInWeek[date.getDay()];
+    const currentTime =date.getHours() + ":" + minutes +" "+ timePeriod;
+
+    //city and current time
     let city = document.getElementById("city");
     let time = document.getElementById("local-time");
+
+    //weather condition
     let weatherDescription =document.getElementById("description");
     let weatherIcon = document.getElementById("icon");
-    let temp = document.getElementById('temp');
-    let tempHigh = document.getElementById('temp-high');
-    let tempLow = document.getElementById('temp-low');
+
+    //temperature and other data
+    let tempElement = document.getElementById('temp');
+    let tempHighElement = document.getElementById('temp-high');
+    let tempLowElement = document.getElementById('temp-low');
     let humidity = document.getElementById('humidity');
     let pressure = document.getElementById('pressure');
-    let windSpeed = document.getElementById('wind-speed');
-     
+
+    //temp celcius fahrenheit buttons
+    let tempUnitCelcius = document.getElementById('temp-unit-cel');
+    let tempUnitFahren = document.getElementById('temp-unit-fahren');
+
+    // temperature units toggle buttons    
+    tempUnitCelcius.addEventListener("click",(e)=>{
+        e.preventDefault();
+        e.target.style.background = '#a2c5ac';
+        tempUnitFahren.style.color = '#a2c5ac';
+        tempUnitCelcius.style.color = '#44394f';
+        tempUnitFahren.style.background = '#ffffff';
+        app.tempUnitConverter("C")
+    });
+   tempUnitFahren.addEventListener("click",(e)=>{
+       e.preventDefault();
+       e.target.style.background = '#a2c5ac';
+       tempUnitCelcius.style.color= '#a2c5ac';
+       tempUnitFahren.style.color = "#44394f";
+      tempUnitCelcius.style.background = "#ffffff";
+      app.tempUnitConverter("F");
+   });
+
+     //http connection
     app.xmlHttpConnection = function(){
             if(window.XMLHttpRequest){
                this.httpCon = new XMLHttpRequest()
@@ -32,6 +60,7 @@
             }
             return this.httpCon;
         }
+        //get user location using HTML5 geolocation API
     app.getUserLocation = function(){
         if(!navigator.geolocation){
             alert("Failed to get your Location");
@@ -40,6 +69,7 @@
             const userLocation = navigator.geolocation.getCurrentPosition(this.userLocationSuccess,this.userLocationError);
         }        
     }
+    //success callback for geolocation in app.getUserLocation()
     app.userLocationSuccess = function(location){        
         const longitude = location.coords.longitude;
         const latitude = location.coords.latitude;
@@ -48,51 +78,57 @@
         app.getWeatherForecast(longitude,latitude);
          
     }
-    
+    //get weather data from fcc-weather-api
     app.getWeatherForecast = function(lon,lat){        
         let connection = this.xmlHttpConnection();
         connection.open("GET",`https://fcc-weather-api.glitch.me/api/current?lat=${lat}&lon=${lon}`);
-        connection.onreadystatechange= app.updateWeatherForecast;
-        connection.send(null);
+        connection.onreadystatechange= function(data){
+             if(this.status == 200 && this.readyState == 4){
+                 const data = JSON.parse(this.responseText);
+                 app.dataArr.push(data);
+                 app.updateWeatherForecast(data);
+                 // hide loading wheel when data is ready and display weather data
+                 document.getElementsByClassName('lds-ring')[0].style.display ="none";
+                 document.getElementsByClassName('weather-data')[0].style.display='flex'; 
+             }
+        };
+        connection.send(null);         
     }
-    app.updateWeatherForecast = function(){
-        if(this.status == 200 && this.readyState == 4){
-            let data = JSON.parse(this.responseText);
+    //display weather data on page
+    app.updateWeatherForecast = function(data){
+       
             city.textContent =data.name + ", " + data.sys.country;
             time.textContent = today + ", " + currentTime; 
             description.textContent = data.weather[0].description;
-
             weatherIcon.setAttribute("src",data.weather[0].icon);
-            temp.textContent= data.main.temp;
-            tempHigh.textContent += data.main.temp_max;
-            tempLow.textContent += data.main.temp_min;
-            pressure.textContent += data.main.pressure;
-            humidity.textContent += data.main.humidity;
-            windSpeed.textContent += data.wind.speed;
-            console.log(data);
-           /* 
-            console.log(data.name);
-            console.log(data.sys.country);
-            console.log("temp " + data.main.temp);
-            console.log("humidity " + data.main.humidity);
-            console.log("pressure " + data.main.pressure);
-            console.log("temp_min " + data.main.temp_min);
-            console.log("temp_max " + data.main.temp_max);
-            console.log("wind" +data.wind.speed);
-            console.log("description - " + data.weather[0].description);
-            console.log("Icon - " + data.weather[0].icon);
-            */
-           
-        }
-         
-          
+            pressure.textContent = data.main.pressure;
+            humidity.textContent = data.main.humidity; 
+            tempElement.textContent= Math.round(data.main.temp); 
+            tempHighElement.textContent = Math.round(data.main.temp_max) +" ° C";
+            tempLowElement.textContent = Math.round(data.main.temp_min) +" ° C";
     }
-
-     
-    app.getUserLocation();
-
     
+   
+    app.tempUnitConverter = function(unit){
+        let tempInCel = app.dataArr[0].main.temp;
+        let tempHigh = app.dataArr[0].main.temp_max;
+        let tempLow = app.dataArr[0].main.temp_min;
+        let tempInCelArr =[tempInCel,tempHigh,tempLow];
+        let tempInFarArr = tempInCelArr.map( (item)=> Math.round((item * (9/5)) + 32));
+        if(unit === "C"){
+          
+            tempElement.textContent= Math.round(tempInCel); 
+            tempHighElement.textContent =  Math.round(tempHigh) + " °C";
+            tempLowElement.textContent =  Math.round(tempLow)+ " °C";
+         }
+        if(unit === "F"){
+            tempElement.textContent= tempInFarArr[0]; 
+            tempHighElement.textContent = tempInFarArr[1] +"° F";
+            tempLowElement.textContent = tempInFarArr[2] +"° F";
+        }
+    }
+    app.getUserLocation();
+   console.log(app.dataArr);  
     //https://fcc-weather-api.glitch.me/api/current?lat=35&lon=139
 }
-
 )();
